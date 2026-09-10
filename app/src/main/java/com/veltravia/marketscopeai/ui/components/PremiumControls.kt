63d7508dd32ba6f3b85f02542f9ff686b9e4d28c
root <root@modal>
2026-09-10 13:26:27 +0000

    Fix build: animateColorAsState lives in androidx.compose.animation, not .core

--- app/src/main/java/com/veltravia/marketscopeai/ui/components/PremiumControls.kt
+++ app/src/main/java/com/veltravia/marketscopeai/ui/components/PremiumControls.kt
@@ -7,7 +7,7 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateColorAsState
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
